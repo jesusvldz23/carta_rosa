d2d891a70ab4696cc65c49fbe0f1478f79f19515
Jesus Valdez <jesusvldz2305@gmail.com>
2026-04-04 15:33:52 -0700

    Arreglo imagenes

--- script.js
+++ script.js
@@ -26,16 +26,16 @@ function mostrarCollage() {
   const collage = document.getElementById("collage");
 
   const fotos = [
-    "foto1.jpg",
-    "foto2.jpg",
-    "foto3.jpg",
-    "foto4.jpg",
-    "foto5.jpg",
-    "foto6.jpg",
-    "foto7.jpg",
-    "foto8.jpg",
-    "foto9.jpg",
-    "foto10.jpg"
+    "foto1.jpeg",
+    "foto2.jpeg",
+    "foto3.jpeg",
+    "foto4.jpeg",
+    "foto5.jpeg",
+    "foto6.jpeg",
+    "foto7.jpeg",
+    "foto8.jpeg",
+    "foto9.jpeg",
+    "foto10.jpeg"
   ];
 
   fotos.forEach((foto, index) => {
